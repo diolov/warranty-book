b709c59d34465bf90addb288701a2b3b0e56e27d
Deyan Iolov <deyan@Demetris-MacBook-Pro-5.local>
2020-01-24 17:14:59 -0500

    update fillable attributes in models

--- app/Attachment.php
+++ app/Attachment.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Attachment extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['fileName', 'thumbnail', 'type'];
 }
